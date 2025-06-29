3a5c99fba625b2858ef9b61b3b6abf15aa6208dd
Yusufcan Var <yusufcanvar90@gmail.com>
2025-06-29 20:59:22 +0300

    feat: Integrate Tailwind CSS and React Router for enhanced styling and routing capabilities

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="h-screen">
+        <h1 className="font-semibold text-slate-800 text-5xl font-geist">
+          Just Do It
+        </h1>
+        <p className="text-slate-500 text-xl font-geist mt-4 leading-relaxed">
+          Time to get things done. <br /> Fast and easy.
+        </p>
+        <button className="btn mt-20">Get Started</button>
+      </div>
     </>
   );
 }
